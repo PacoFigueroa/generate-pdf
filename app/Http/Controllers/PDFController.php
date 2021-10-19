f373a512d643bb10075bcda01306b4a3871ed0f3
Francisco Figueroa <francisco.figueroa@clickittech.com.mx>
2021-10-19 18:03:00 -0500

    update

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use PDF;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class PDFController extends Controller
@@ -10,8 +11,57 @@ class PDFController extends Controller
     // Generate PDF
     public function createPDF()
     {
+        $data = [
+            'payment' => [
+                [
+                    'transaction_id' => 'TRN-KF99-MA9J',
+                    'tin' => '2352344433',
+                    'amount' => 'k32,300.50',
+                    'comment' => 'Augut 2021 GST tax comment',
+                    'type' => 'Goods and Service Tax',
+                    'year' => '2021',
+                    'month' => 'September',
+                    'assessment_number' => '485982'
+                ],
+                [
+                    'transaction_id' => 'TRN-KF99-MA9J',
+                    'tin' => '2352344433',
+                    'amount' => 'k32,300.50',
+                    'comment' => 'n/a',
+                    'type' => 'Goods and Service Tax',
+                    'year' => '2021',
+                    'month' => 'September',
+                    'assessment_number' => '485982'
+                ],
+                [
+                    'transaction_id' => 'TRN-KF99-MA9J',
+                    'tin' => '2352344433',
+                    'amount' => 'k32,300.50',
+                    'comment' => 'n/a',
+                    'type' => 'Goods and Service Tax',
+                    'year' => 'n/a',
+                    'month' => '',
+                    'assessment_number' => '485982'
+                ]
+            ],
+            'email_to' => 'john.citizen@gmail.com',
+            'receipt_to' => [
+                'name' => 'Geraint',
+                'surname' => 'Davidson',
+                'billing_name' => 'Dav & Sons. Machinery',
+                'address' => ' Section 32, Lot 19, Saraga Street',
+                'address_2' => 'Six Mile',
+                'city' => 'Port Moresby',
+                'province' => 'National Capital District',
+                'zip' => 121,
+                'country' => 'Papua New Guinea'
+            ]
+        ];
+
         // return view('api.pdf.payment-receipt');
-        $pdf = PDF::loadView('pdf');
+        $pdf = PDF::loadView('pdf', array(
+            'data' => $data
+        ));
 
         // download PDF file with download method
         return $pdf->stream('pdf_file.pdf');
--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -6,218 +6,124 @@
     <link href="css/pdf.css" rel="stylesheet">
 </head>
 <body>
-    <div class="page">
-        <div class="container">
-            <h1 class="test-title">PAPUA NEW GUINEA INTERNAL REVENUE COMMISSION</h1>
-            <h1 class="test-title2">Payment Receipt</h1>
-            <p class="thanks">Thank you for your payment to the <strong>Internal Revenu Commission.</strong> This receipt is available anytime from your myIRC account.</p>
-            {{-- <div>
-                <p class="receipt">Receipt to:</p>
-               <div class="box">
-                    <div class="left">
-                        <div class="content">
-                            <span>Given Name(s):</span><br>
-                            <span>Surname:</span><br>
-                            <span>Billing name:</span><br>
-                            <span>Address (Line 1):</span><br>
-                            <span>Address (Line 2):</span><br>
-                            <span>City/Town:</span><br>
-                            <span>Province/State/Region:</span><br>
-                            <span>Postal Code/ZIP:</span><br>
-                            <span>Country:</span><br>
-                        </div>
-                    </div>
-                    <div class="right">
-                        <div class="content">
-                            <span>Geraint</span><br>
-                            <span>Davidson</span><br>
-                            <span>Dav & Sons. Machinery</span><br>
-                            <span>Section 32, Lot 19, Saraga Street</span><br>
-                            <span>Six Mile</span><br>
-                            <span>Port Moresby</span><br>
-                            <span>National Capital District</span><br>
-                            <span>121</span><br>
-                            <span>Papua New Guinea</span><br>
-                        </div>
-                    </div>
-                </div>
-                <div class="box-2">
-                    <div class="left-2">
-                        <div class="content-2">
-                            <span class="confirm">Confirmation sent to:</span>
-                            <span class="email">john.citizen@gmail.com</span>
-                        </div>
-                    </div>
-                    <div class="right-2">
-                        <div class="content-2">
-                            <span>QR</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="rectangle-receipt-2">
-                    <span>Confirmation sent to:</span>
-                    <span>john.citizen@gmail.com</span>
-                    <span>QR</span>
-                </div>
+    <div id="main-container" >
+        <p id="heading-text" style="margin-bottom:0;">PAPUA NEW GUINEA INTERNAL REVENUE COMMISSION</p>
+        <h2 class="purple-title" style="margin-bottom:0;">Payment Receipt</h2>
+        <p class="normal-text">Thank you for your payment to the <span class="bold">Internal Revenue Commission.</span> This receipt is available anytime from your myIRC account.</p>
+        <h3 class="purple-title" style="margin-bottom:0;">Receipt to:</h3>
+        <div class="blue-box m-tb-20">
+            <div class="left" style="margin-top: 30px;">
+                <p class="bold normal-text">Given Name(s):</p>
+                <p class="bold normal-text">Surname:</p>
+                <p class="bold normal-text">Billing name:</p>
+                <p class="bold normal-text">Address (Line 1):</p>
+                <p class="bold normal-text">Address (Line 2):</p>
+                <p class="bold normal-text">City/Town:</p>
+                <p class="bold normal-text">Province/State/Region:</p>
+                <p class="bold normal-text">Postal Code/ZIP:</p>
+                <p class="bold normal-text">Country:</p>
             </div>
-            <div>
-                <p class="your-payment">Your payment breakdown:</p>
-                <div class="your-payment-table">
-                    <div class="your-payment-1">
-                        <div>Transaction ID</div>
-                        <div>TIN Amount</div>
-                        <div>Comment</div>
-                        <div>Type</div>
-                        <div>Year Mounth</div>
-                    </div>
-                    <div class="your-payment-2">
-                        <div>TRN-KF99-MA9J</div>
-                        <div>2352344433 K32,300.50</div>
-                        <div>August 2021 GST tax comment</div>
-                        <div>Goods and Service Tax</div>
-                        <div>2021 September</div>
-                    </div>
-                    <div>
-                        Assessment Number 485982
-                    </div>
-                    <div class="your-payment-3">
-                        <div>TRN-KKKD-993A</div>
-                        <div>2352344433 K32,300.50</div>
-                        <div>n/a</div>
-                        <div>Corporate Income Tax (Non-Residents)</div>
-                        <div>2021 July</div>
-                    </div>
-                    <div>
-                        Assessment Number 485982
-                    </div>
-                    <div>
-                        <div>TRN-LL39-D234</div>
-                        <div>2352344433 K32,300.50</div>
-                        <div>n/a</div>
-                        <div>GST Late Penaty</div>
-                        <div>n/a</div>
-                    </div>
-                    <div>
-                        n/a
-                    </div>
-                    <div>
-                        Total: K5000.00
-                    </div>
-                </div>
-                <table>
-                    <tr>
-                        <th>Transaction ID</th>
-                        <th>Tin Amount</th>
-                        <th>Comment</th>
-                        <th>Type</th>
-                        <th>Year Month</th>
-                    </tr>
-                    <tr>
-                        <td>TRN-KF99-MA9J Assessment Number</td>
-                        <td>2352344433 k32,300.50</td>
-                        <td>Augut 2021 GST tax comment</td>
-                        <td>Goods and Service Tax</td>
-                    </tr>
-                    <tr>
-                        <td>Centro comercial Moctezuma</td>
-                        <td>Francisco Chang</td>
-                        <td>Mexico</td>
-                        <td>Germany</td>
-                    </tr>
-                    <tr>
-                        <td>Ernst Handel</td>
-                        <td>Roland Mendel</td>
-                        <td>Austria</td>
-                        <td>Germany</td>
-                    </tr>
-                    <tr>
-                        <td>Island Trading</td>
-                        <td>Helen Bennett</td>
-                        <td>UK</td>
-                        <td>Germany</td>
-                    </tr>
-                    <tr>
-                        <td>Laughing Bacchus Winecellars</td>
-                        <td>Yoshi Tannamuri</td>
-                        <td>Canada</td>
-                    </tr>
-                    <tr>
-                        <td>Magazzini Alimentari Riuniti</td>
-                        <td>Giovanni Rovelli</td>
-                        <td>Italy</td>
-                    </tr>
-                </table>
+            <div class="right" style="margin-top: 30px;">
+                @foreach ($data['receipt_to'] as $item)
+                    <p class="normal-text">{{ $item }}</p>
+                @endforeach
             </div>
-            <div>
-                <p class="access">Access to your information:</p>
-                <p class="access-text">Did you know that your payment history and doenloadable receipts are available 24/7 from your myIRC account. Your myIRC account also allows you to make directo contact with officers from the Internal Revenue commission</p>
-                <a class="access-link" href="#">https://irc.gov.pg/myirc</a>
-            </div>
-            <div>
-                <p class="support">Support Centre:</p>
-                <p class="support-text">You can contact an officer from the Interal Revenue Commission by using the myIRC Support Centre, accessible 24/7. Get your questions answered quickly, without the hassle.</p>
-                <a class="support-link" href="#">https://irc.gov.pg/support-centre</a>
-            </div> --}}
         </div>
-        <div class="right-side">
-            <h1>righ</h1>
-            {{-- <div class="logo"></div>
-            <div class="pngirc">
-                <h3>Papua New Guinea</h3>
-                <h3>Internal Revenue Commission</h3>
+        <div class="blue-box m-tb-20">
+            <div class="right">
+                <p class="purple-title bold underline" >Confirmation sent to:</p>
+                <p class="link-text">{{ $data['email_to'] }}</p>
             </div>
-            <h1 class="building">Building the Foundation of a Robust.</h1>
-            <h1 class="building">Moden and Efficient Tax Administration</h1>
-            <div>
-                <h1 class="date">Date of payment:</h1>
-                <div class="rectangle-date">
-                    28/03/2021
-                </div>
+            <div class="left">
+                QR
             </div>
-            <div>
-                <h1 class="total">Total amount paid:</h1>
-                <div class="rectangle-total">
-                    K1,203.98
-                </div>
+        </div>
+        <h3 class="purple-title" style="margin-bottom:0;">Your payment breakdown:</h3>
+        <table id="maintable">
+            <thead style="border-bottom: 2px solid #2782f9;">
+                <tr>
+                    <th>Transaction ID</th>
+                    <th>TIN <span class="bold">Amount </span></th>
+                    <th>Comment</th>
+                    <th>Type</th>
+                    <th>Year <span class="bold">Month </span></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($data['payment'] as $payment)
+                    <tr>
+                        <td style="height: 50px;">{{ $payment['transaction_id'] }}</td>
+                        <td>{{ $payment['tin'] }} <span class="bold">{{ $payment['amount'] }}</span></td>
+                        <td>{{ $payment['comment'] }}</td>
+                        <td>{{ $payment['type'] }}</td>
+                        <td>{{ $payment['year'] }} <span class="bold">{{ $payment['month'] }}</span></td>
+                    </tr>
+                    <tr class="border-bottom-blue">
+                        <td style="height: 50px;"><span class="bold" style="margin-top:10px;">Assessment Number:</span></td>
+                        <td colspan="4">{{ $payment['assessment_number'] }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        <div id="total-table">
+            <div class="left">
+                <p class="bold normal-text">Total:</p>
             </div>
-            <div>
-                <h1 class="payment">Payment ID:</h1>
-                <div class="rectangle-payment">
-                    28/03/2021
-                </div>
+            <div class="right">
+                <p class="bold normal-text">3000</p>
             </div>
-            <div>
-                <h1 class="bank">Bank Approval:</h1>
-                <div class="rectangle-bank">
-                    234652333
-                </div>
+        </div>
+        <h4 class="purple-title" style="margin-bottom:0;">Access to your information:</h4>
+        <div>
+            <p class="info-text" style="margin:0;">Did you know that your payment history and downloadable receipts are available 24/7 from your myIRC account. Your myIRC account also allows you to make direct contact with officers from the Internal Revenue Commission</p>
+            <a class="info-text link-text underline" href="#">https://irc.gov.pg/myirc</a>
+        </div>
+        <h4 class="purple-title" style="margin-bottom:0;">Support Centre:</h4>
+        <div>
+            <p class="info-text" style="margin:0;">You can contact an officer from the Internal Revenue Commission by using the myIRC Support Centre, accessible 24/7. Get your questions answered quickly, without the hassle.</p>
+            <a class="info-text link-text underline" href="#">https://irc.gov.pg/support-centre</a>
+        </div>
+    </div>
+    <div id="sidebar">
+        <h1 style="text-align:center;">image</h1>
+        <p class="normal-text" style="text-align:center;">PAPUA NEW GUINEA INTERNAL REVENUE COMMISSION</p>
+        <p class="normal-text" style="font-style: italic; text-align:center;" >Building the Foundation of a Robust, Modern and Efficient Tax Administration</p>
+        <div class="sidebar-inputs">
+            <p class="normal-text bold">Date of payment:</p>
+            <div class="fake-input">TEST</div>
+        </div>
+        <div class="sidebar-inputs">
+            <p class="normal-text bold">Total amount paid:</p>
+            <div class="fake-input"> TEST</div>
+        </div>
+        <div class="sidebar-inputs">
+            <p class="normal-text bold">Payment ID:</p>
+            <div class="fake-input"> TEST</div>
+        </div>
+        <div class="sidebar-inputs">
+            <p class="normal-text bold">Bank Approval:</p>
+            <div class="fake-input"> TEST</div>
+        </div>
+        <div class="sidebar-inputs">
+            <p class="normal-text bold">Payment Type:</p>
+            <div class="fake-input"> TEST</div>
+        </div>
+        <div class="sidebar-inputs">
+            <p class="normal-text bold">Status:</p>
+            <div class="fake-input"> TEST</div>
+        </div>
+        <p class="normal-text" style="text-align:center;">Page 1 of 1</p>
+        <div class="sidebar-inputs">
+            <div class="sidebar-info">
+                <p class="purple-title bold">Website</p>
+                <a class="info-text link-text underline" href="#">irc.gov.pg</a>
+                <p class="purple-title bold">My iRC</p>
+                <a class="info-text link-text underline" href="#">irc.gov.pg/myirc</a>
+                <p class="purple-title bold" >Postal:</p>
+                <p>P.O Box 777</p>
+                <p>Port Moresby, NCD</p>
+                <p>Papua New Guinea</p>
             </div>
-            <div>
-                <h1 class="payment-type">Payment Type:</h1>
-                <div class="rectangle-payment-type">
-                    Card
-                </div>
-            </div>
-            <p class="pagination">Page 1 of 1</p>
-            <div class="rectangle-info">
-                <div>
-                    <h1 class="website">Website</h1>
-                    <h2 class="link"><a href="#">irc.gov.pg</a></h2>
-                </div>
-                <div>
-                    <h1 class="myIRC">myIRC</h1>
-                    <h2 class="link"><a href="#">irc.gov.pg/myirc</a></h2>
-                </div>
-                <div class="rectangle-info">
-                    <h1 class="postal">Postal</h1>
-                    <h2>P.O Box 777</h2>
-                    <h2>Port Moresby, NCD</h2>
-                    <h2>Papua New Guinea</h2>
-                </div>
-                <div>
-                    <img src="" alt="LOGO2">
-                </div>
-            </div> --}}
         </div>
     </div>
 </body>
